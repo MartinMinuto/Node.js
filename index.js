546f19827ec263a7ecd17dd245768e65f0e269cb
MartinMinuto <martin.minuto1@gmail.com>
2023-05-04 18:22:21 -0300

    1.2

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import usuarioRoutes from './routes/usuarioRoutes'
+import usuarioRoutes from './routes/usuarioRoutes.js'
 
 const app = express()
 
 app.use('/auth', usuarioRoutes)
+
+app.use(express.static('public'))
 
 app.set('view engine', 'pug')
 app.set('views', './views')
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,9 +1,10 @@
 import express from "express"
+import { formularioLogin } from "../controllers/usuarioController";
+import { formularioRegistro } from "../controllers/usuarioController";
 
 const router = express.Router();
 
-router.get('/login', (req, res) => {
-    res.render('auth/login')
-})
+router.get('/login', formularioLogin)
+router.get('/registro', formularioRegistro)
 
 export default router
