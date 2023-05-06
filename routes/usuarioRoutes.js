91760e11e78e8ba6f6f46c744159cbe1477b5cf3
MartinMinuto <martin.minuto1@gmail.com>
2023-05-05 21:17:38 -0300

    1.2.3

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,10 +1,10 @@
 import express from "express"
-import { formularioLogin } from "../controllers/usuarioController";
-import { formularioRegistro } from "../controllers/usuarioController";
+import { formularioLogin, formularioRegistro, formularioOlvidePassword } from "../controllers/usuarioController.js";
 
 const router = express.Router();
 
 router.get('/login', formularioLogin)
 router.get('/registro', formularioRegistro)
+router.get('/olvide-password', formularioOlvidePassword)
 
 export default router
