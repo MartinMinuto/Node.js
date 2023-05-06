5f7c0ae92da28ed992bc3973c81a4e0526151a86
MartinMinuto <martin.minuto1@gmail.com>
2023-05-06 14:38:24 -0300

    1.3.1

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,3 +1,4 @@
+import { check, validationResult } from 'express-validator'
 import Usuario from '../model/Usuario.js'
 
 const formularioLogin = (req, res) => {
@@ -13,8 +14,20 @@ const formularioRegistro = (req, res) => {
 }
 
 const registrar = async (req, res) => {
-   const usuario = await Usuario.create(req.body)
+   await check('nombre').notEmpty().withMessage('Este campo no puede ir vacio!').run(req)
+   await check('email').isEmail().withMessage('Este campo no puede ir vacio!').run(req)
+   await check('password').isLength({min: 6}).withMessage('La contraseña debe tener 6 caracteres como minimo').run(req)
+   await check('repetir_password').equals('password').withMessage('La contraseña no es la misma').run(req)
+   let resultado = validationResult(req)
 
+   if(!resultado.isEmpty()){
+      return res.render('auth/registro', {
+        pagina: 'Crear Cuenta',
+        errores: resultado.array()
+      })
+   }
+
+   const usuario = await Usuario.create(req.body)
    res.json(usuario)
 }
 
