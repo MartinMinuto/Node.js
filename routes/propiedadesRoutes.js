54454a74f6ef31500da0e154f293499582645e20
MartinMinuto <martin.minuto1@gmail.com>
2023-05-21 22:53:42 -0300

    Proteger rutas

--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { body } from 'express-validator'
 import { admin, crear, guardar } from '../controllers/propiedadController.js'
+import protegerRuta from '../middleware/protegerRuta.js'
 
 const router = express.Router()
 
-router.get('/mis-propiedades', admin)
-router.get('/propiedades/crear', crear)
+router.get('/mis-propiedades', protegerRuta, admin)
+router.get('/propiedades/crear', protegerRuta, crear)
 router.post('/propiedades/crear',
     body('titulo').notEmpty().withMessage('El Titulo del anuncio es obligatorio'),
     body('description').notEmpty().withMessage('La descripcion no puede ir vacia').isLength({max:200}).withMessage('La desciption es muy larga'),
