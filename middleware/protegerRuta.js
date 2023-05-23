17a371802b61663824e57437f2da9268486e3402
MartinMinuto <martin.minuto1@gmail.com>
2023-05-22 22:14:36 -0300

    15.8

--- middleware/protegerRuta.js
+++ middleware/protegerRuta.js
@@ -1,7 +1,17 @@
-
+import jwt from 'jsonwebtoken'
 
 const protegerRuta = async (req, res, next) => {
 
+    const { _token } = req.cookies
+    if(!_token){
+        return res.redirect('/auth/login')
+    }
+
+    try{
+
+    } catch (error) {
+        return res.clearCookie('_token').redirect('/auth/login')
+    }
 
     next()
 }
