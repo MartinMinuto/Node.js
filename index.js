384a9a877a5454d0300b6acaea8a7972b8de0aea
MartinMinuto <martin.minuto1@gmail.com>
2023-06-09 18:44:51 -0300

    1.7.6

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import csrf from 'csurf';
 import cookieParser from 'cookie-parser';
 import usuarioRoutes from './routes/usuarioRoutes.js'
 import propiedadesRoutes from './routes/propiedadesRoutes.js'
+import appRoutes from './routes/appRoutes.js'
 import db from './config/db.js'
 
 const app = express()
@@ -21,6 +22,7 @@ try{
    console.log(error)
 }
 
+app.use('/', appRoutes)
 app.use('/auth', usuarioRoutes)
 app.use('/', propiedadesRoutes)
 
