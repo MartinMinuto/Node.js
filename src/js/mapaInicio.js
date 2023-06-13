a4f63096ef0485a85b6f6e3f393b9cc6af9fadd8
MartinMinuto <martin.minuto1@gmail.com>
2023-06-13 18:43:50 -0300

    1.7.12

--- src/js/mapaInicio.js
+++ src/js/mapaInicio.js
@@ -1,7 +1,9 @@
 (function(){
     const lat = -34.5555447;
     const lng = -58.4823779;
-    const mapa = L.map('mapa-inicio').setView([lat, lng ], 15);
+    const mapa = L.map('mapa-inicio').setView([lat,lng], 13);
+
+    let markers = new L.FeatureGroup().addTo(mapa)
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -12,10 +14,22 @@
             const url = '/api/propiedades'
             const respuesta = await fetch(url)
             const propiedades = await respuesta.json()
-
+            mostrarPropiedades(propiedades)
         } catch (error) {
             console.log(error)
         }
     }
 
-})
+    const mostrarPropiedades = propiedades => {
+        propiedades.forEach(propiedad => {
+            const marker = new L.marker([propiedad?.lat, propiedad?.lng], {
+                autoPan: true
+            })
+            .addTo(mapa)
+            .bindPopup('informacion')
+        })
+    }
+
+    obtenerPropiedades()
+
+})()
