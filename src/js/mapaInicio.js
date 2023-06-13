b540a7999faf00bc8f5154a58c87ceadc1433184
MartinMinuto <martin.minuto1@gmail.com>
2023-06-12 22:26:36 -0300

    1.7.12

--- src/js/mapaInicio.js
+++ src/js/mapaInicio.js
@@ -7,4 +7,15 @@
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mapa);
 
+    const obtenerPropiedades = async () => {
+        try {
+            const url = '/api/propiedades'
+            const respuesta = await fetch(url)
+            const propiedades = await respuesta.json()
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
 })
