ac952fd103231a7e7050aec6fd6cf096d768d0fb
MartinMinuto <martin.minuto1@gmail.com>
2023-06-22 17:08:19 -0300

    1.8.5

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -302,7 +302,8 @@ const mostrarPropiedad = async (req,res) => {
     res.render('propiedades/mostrar', {
         propiedad,
         pagina: propiedad.titulo,
-        csrfToken: req.csrfToken()
+        csrfToken: req.csrfToken(),
+        usuario: req.usuario
     })
 
 }
--- middleware/identificarUsuario.js
+++ middleware/identificarUsuario.js
@@ -3,10 +3,23 @@ import Usuario from '../model/Usuario.js'
 
 const indentificarUsuario = async (req,res, next) => {
 
-    const token = req.cookies._token
-    if(!token) {
+    const {_token} = req.cookies
+    if(!_token) {
         req.usuario = null
         return next()
+    }
+
+    try{
+        const decoded = jwt.verify(_token, process.env.JWT_SECRET)
+        const usuario = await Usuario.scope('eliminarPassword').findByPK(decoded.id)
+
+        if(usuario) {
+            req.usuario = usuario
+        }
+        return next();
+    } catch(error) {
+        console.log(error)
+        return res.clearCookie('_token').redirect('/auth/login')
     }
 
 }
